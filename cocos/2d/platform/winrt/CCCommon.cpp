32449d22f94c29d4605baa43d1a5517275c86283
Dale Stammen <dalestam@microsoft.com>
2014-03-05 10:39:35 -0800

    removed CCLog and WP8 C++ modalwindow

--- cocos/2d/platform/winrt/CCCommon.cpp
+++ cocos/2d/platform/winrt/CCCommon.cpp
@@ -27,8 +27,7 @@ THE SOFTWARE.
 #include "CCWinRTUtils.h"
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8)
-#include "ModalLayer.h"
-#include "CCGLView.h"
+#include "platform/wp8/CCGLView.h"
 #endif
 
 #if defined(VLD_DEBUG_MEMORY)
@@ -36,28 +35,6 @@ THE SOFTWARE.
 #endif
 
 NS_CC_BEGIN
-
-#define MAX_LEN         (cocos2d::kMaxLogLen + 1)
-
-void CCLog(const char * pszFormat, ...)
-{
-#if defined(COCOS2D_DEBUG)
-    char szBuf[MAX_LEN];
-
-    va_list ap;
-    va_start(ap, pszFormat);
-    vsnprintf_s(szBuf, MAX_LEN, MAX_LEN, pszFormat, ap);
-    va_end(ap);
-
-    WCHAR wszBuf[MAX_LEN] = {0};
-    MultiByteToWideChar(CP_UTF8, 0, szBuf, -1, wszBuf, sizeof(wszBuf));
-    OutputDebugStringW(wszBuf);
-    OutputDebugStringW(L"\n");
-
-    WideCharToMultiByte(CP_ACP, 0, wszBuf, sizeof(wszBuf), szBuf, sizeof(szBuf), NULL, FALSE);
-    printf("%s\n", szBuf);
-#endif
-}
 
 
 void CCMessageBox(const char * pszMsg, const char * pszTitle)
@@ -78,14 +55,7 @@ void CCMessageBox(const char * pszMsg, const char * pszTitle)
     // Show the message dialog
     msg->ShowAsync();
 #else
-
-    CCGLView* pEGLView = CCGLView::sharedOpenGLView();
-    if(!pEGLView->ShowMessageBox(title, message))
-    {
-	    ModalLayer *messageBox = ModalLayer::create();
-	    messageBox->setMessage(pszMsg);
-	    CCDirector::sharedDirector()->getRunningScene()->addChild(messageBox);
-    }
+    GLView::sharedOpenGLView()->ShowMessageBox(title, message);
 #endif
 
 }
