a505a8ec423ad0b19093a127a1ef421a07dbb91e
Dale Stammen <dalestam@microsoft.com>
2014-03-06 19:29:17 -0800

    use CCGL.h

--- cocos/2d/platform/wp8-xaml/DirectXBase.h
+++ cocos/2d/platform/wp8-xaml/DirectXBase.h
@@ -28,12 +28,7 @@ THE SOFTWARE.
 #include <d3d11_1.h>
 #include <DirectXMath.h>
 #include "DirectXHelper.h"
-#include "EGL/egl.h"
-#include "EGL/eglext.h"
-#include "EGL/eglplatform.h"
-#include "GLES2/gl2.h"
-#include "GLES2/gl2ext.h"
-#include "../src/common/winrtangle.h"
+#include "CCGL.h"
 
 // Helper class that initializes DirectX APIs for 3D rendering.
 ref class DirectXBase abstract
