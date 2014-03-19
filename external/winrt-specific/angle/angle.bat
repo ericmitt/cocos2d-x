755a714125a0de9c25f4384036b5ae816ccf3f7a
Dale Stammen <dalestam@microsoft.com>
2014-03-19 11:21:01 -0700

    added angle file copy script

---
+++ external/winrt-specific/angle/angle.bat
@@ -0,0 +1,22 @@
+xcopy ".\..\angleproject\src\WP8\redist\vs2012\ARM\Release\libGLESv2_phone\libGLESv2_phone.dll" ".\..\..\wp8-specific\angle\prebuilt\ARM\" /iycq
+xcopy ".\..\angleproject\src\WP8\redist\vs2012\ARM\Release\libGLESv2_phone\libGLESv2_phone.lib" ".\..\..\wp8-specific\angle\prebuilt\ARM\" /iycq
+xcopy ".\..\angleproject\src\WP8\redist\vs2012\ARM\Release\libGLESv2_phone\libGLESv2_phone.exp" ".\..\..\wp8-specific\angle\prebuilt\ARM\" /iycq
+xcopy ".\..\angleproject\src\WP8\redist\vs2012\ARM\Release\libEGL_phone\libEGL_phone.dll" ".\..\..\wp8-specific\angle\prebuilt\ARM\" /iycq
+xcopy ".\..\angleproject\src\WP8\redist\vs2012\ARM\Release\libEGL_phone\libEGL_phone.lib" ".\..\..\wp8-specific\angle\prebuilt\ARM\" /iycq
+xcopy ".\..\angleproject\src\WP8\redist\vs2012\ARM\Release\libEGL_phone\libEGL_phone.exp" ".\..\..\wp8-specific\angle\prebuilt\ARM\" /iycq
+
+xcopy ".\..\angleproject\src\WP8\redist\vs2012\Win32\Release\libGLESv2_phone\libGLESv2_phone.dll" ".\..\..\wp8-specific\angle\prebuilt\Win32\" /iycq
+xcopy ".\..\angleproject\src\WP8\redist\vs2012\Win32\Release\libGLESv2_phone\libGLESv2_phone.lib" ".\..\..\wp8-specific\angle\prebuilt\Win32\" /iycq
+xcopy ".\..\angleproject\src\WP8\redist\vs2012\Win32\Release\libGLESv2_phone\libGLESv2_phone.exp" ".\..\..\wp8-specific\angle\prebuilt\Win32\" /iycq
+xcopy ".\..\angleproject\src\WP8\redist\vs2012\Win32\Release\libEGL_phone\libEGL_phone.dll" ".\..\..\wp8-specific\angle\prebuilt\Win32\" /iycq
+xcopy ".\..\angleproject\src\WP8\redist\vs2012\Win32\Release\libEGL_phone\libEGL_phone.lib" ".\..\..\wp8-specific\angle\prebuilt\Win32\" /iycq
+xcopy ".\..\angleproject\src\WP8\redist\vs2012\Win32\Release\libEGL_phone\libEGL_phone.exp" ".\..\..\wp8-specific\angle\prebuilt\Win32\" /iycq
+
+xcopy ".\..\angleproject\include" "include" /eiycq
+
+xcopy ".\..\angleproject\src\common\winrtangle.h" "include" /iycq
+xcopy ".\..\angleproject\src\common\winrtangleutils.h" "include" /iycq
+xcopy ".\..\angleproject\src\common\winrtplatform.h" "include" /iycq
+xcopy ".\..\angleproject\src\common\angleutils.h" "include" /iycq
+xcopy ".\..\angleproject\samples\gles2_book\Common\esUtil.h" "include" /iycq
+
